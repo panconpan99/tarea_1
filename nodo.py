02220abfd106aa9c629762de0a2812d76e06cead
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2022-08-30 22:37:24 -0400

    añadir tarea_1

---
+++ nodo.py
@@ -0,0 +1,23 @@
+class nodo_estado:
+    def __init__(self, V, EP, A, N):
+        self.valor = V
+        self.padre = EP
+        self.accion = A
+        self.nivel = N
+
+    def get_valor(self):
+        return self.valor
+
+    def get_nivel(self):
+        return self.nivel
+
+    def get_accion(self):
+        return self.accion
+    
+    def get_padre(self):
+        return self.padre
+    
+    def __eq__(self, n):
+        if n is None:
+            return False
+        return self.valor == n.get_valor()
